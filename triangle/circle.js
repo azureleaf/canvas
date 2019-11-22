f7d0e04b73f71acf2a30bd84979bf63068804b99
akb <doge.wow.so.scare@gmail.com>
2019-11-22 20:23:17 +0900

    chore: Add comments to clarify

--- triangle/circle.js
+++ triangle/circle.js
@@ -8,12 +8,10 @@ class Point {
   }
 }
 
-/* Global variables*/
-let clickLoc = {
-  x: [],
-  y: []
-};
+// ユーザーがクリックした位置を保持する配列（最大で３要素入る）
+let clickPoints = [];
 
+// Note that these 2 values must be identical with their equivalents in HTML
 const CANVAS_WIDTH = 800;
 const CANVAS_HEIGHT = 512;
 
@@ -24,7 +22,7 @@ function draw(x1, y1, x2, y2, x3, y3, content = "all") {
   }
   var ctx = canvas.getContext("2d");
 
-  // Config canvas size & resolution
+  // Configure canvas size & resolution
   const dpr = window.devicePixelRatio || 1; // This Web API is supported by most browser
   canvas.width = CANVAS_WIDTH * dpr;
   canvas.height = CANVAS_HEIGHT * dpr;
@@ -32,7 +30,7 @@ function draw(x1, y1, x2, y2, x3, y3, content = "all") {
   canvas.style.width = CANVAS_WIDTH + "px";
   canvas.style.height = CANVAS_HEIGHT + "px";
 
-  // 三角形描画
+  // 三角形の描画
   ctx.beginPath();
   ctx.moveTo(x1, y1);
   ctx.lineTo(x2, y2);
@@ -64,96 +62,31 @@ function draw(x1, y1, x2, y2, x3, y3, content = "all") {
 
   // Conditional rendering
   // "content"のキーワードによって、指定された円だけを描写するのか、あるいは全てを表示するかを切り替える
-  if (content == "centroid" || content == "all") {
-    drawCentroid(
-      params.centroid,
-      params.r,
-      params.R,
-      params.S,
-      x1,
-      y1,
-      x2,
-      y2,
-      x3,
-      y3,
-      ctx
-    );
-  }
-
-  if (content == "circumcenter" || content == "all") {
-    drawCircumcenter(
-      params.circumcenter,
-      params.r,
-      params.R,
-      params.S,
-      x1,
-      y1,
-      x2,
-      y2,
-      x3,
-      y3,
-      ctx
-    );
-  }
-  if (content == "excenter" || content == "all") {
-    drawExcenter(
-      params.excenter,
-      params.r,
-      params.R,
-      params.S,
-      params.edgePoints,
-      x1,
-      y1,
-      x2,
-      y2,
-      x3,
-      y3,
-      ctx
-    );
-  }
-  if (content == "orthocenter" || content == "all") {
-    drawOrthocenter(
-      params.orthocenter,
-      params.r,
-      params.R,
-      params.S,
-      x1,
-      y1,
-      x2,
-      y2,
-      x3,
-      y3,
-      ctx
-    );
-  }
-  if (content == "incenter" || content == "all") {
-    drawIncenter(
-      params.incenter,
-      params.r,
-      params.R,
-      params.S,
-      x1,
-      y1,
-      x2,
-      y2,
-      x3,
-      y3,
-      ctx
-    );
-  }
+  if (content == "incenter" || content == "all") drawIncenter(params, ctx);
+  if (content == "circumcenter" || content == "all")
+    drawCircumcenter(params, ctx);
+  if (content == "centroid" || content == "all")
+    drawCentroid(params, x1, y1, x2, y2, x3, y3, ctx);
+  if (content == "excenter" || content == "all")
+    drawExcenter(params, x1, y1, x2, y2, x3, y3, ctx);
+  if (content == "orthocenter" || content == "all")
+    drawOrthocenter(params, x1, y1, x2, y2, x3, y3, ctx);
 
   // オイラー線は常に表示する
-  if (true) {
-    drawEulerLine(
-      ctx,
-      params.circumcenter,
-      params.centroid,
-      params.orthocenter
-    );
-  }
+  drawEulerLine(ctx, params);
 }
 
-// 三角形の三点座標を受け取り、五心とそれに関する全ての変数を計算し返す
+/**
+ * 三角形の三点座標を受け取り、それを基に五心に関する全変数を計算
+ *
+ * @param {Number} x1
+ * @param {Number} y1
+ * @param {Number} x2
+ * @param {Number} y2
+ * @param {Number} x3
+ * @param {Number} y3
+ * @return {object} 五心の中心座標、円の半径などの変数
+ */
 function calcParams(x1, y1, x2, y2, x3, y3) {
   // ３辺の長さ (Pythagorean theorem)
   var c = Math.pow(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2), 1 / 2);
@@ -178,7 +111,10 @@ function calcParams(x1, y1, x2, y2, x3, y3) {
   var R = (a * b * c) / (4 * r * s);
   document.getElementById("R").value = R;
 
-  // 内心 Incenter の位置計算
+  /**
+   * 内心 Incenter の位置計算
+   */
+
   var incenter = new Point(
     (a * x1 + b * x2 + c * x3) / (a + b + c),
     (a * y1 + b * y2 + c * y3) / (a + b + c)
@@ -187,7 +123,9 @@ function calcParams(x1, y1, x2, y2, x3, y3) {
   document.getElementById("x0i").value = incenter.x;
   document.getElementById("y0i").value = incenter.y;
 
-  // 外心 Circumcenter の位置計算
+  /**
+   * 外心 Circumcenter の位置計算
+   */
   var circumcenter = new Point(
     (a * a * (b * b + c * c - a * a) * x1 +
       b * b * (c * c + a * a - b * b) * x2 +
@@ -202,13 +140,18 @@ function calcParams(x1, y1, x2, y2, x3, y3) {
   document.getElementById("x0O").value = circumcenter.x;
   document.getElementById("y0O").value = circumcenter.y;
 
-  // 重心 Centroid の位置計算
+  /**
+   * 重心 Centroid の位置計算
+   */
   var centroid = new Point((x1 + x2 + x3) / 3, (y1 + y2 + y3) / 3);
 
   document.getElementById("x0g").value = centroid.x;
   document.getElementById("y0g").value = centroid.y;
 
-  // 垂心 Orthocenter の位置計算
+  /**
+   * 垂心 Orthocenter の位置計算
+   */
+  // ３つの角度を辺長から計算
   var thetaA = Math.acos(
     ((x2 - x1) * (x3 - x1) + (y2 - y1) * (y3 - y1)) / (b * c)
   );
@@ -219,6 +162,7 @@ function calcParams(x1, y1, x2, y2, x3, y3) {
     ((x1 - x3) * (x2 - x3) + (y1 - y3) * (y2 - y3)) / (a * b)
   );
 
+  // 幾何関係から垂心の位置を計算
   var orthocenter = new Point(
     (Math.tan(thetaA) * x1 + Math.tan(thetaB) * x2 + Math.tan(thetaC) * x3) /
       (Math.tan(thetaA) + Math.tan(thetaB) + Math.tan(thetaC)),
@@ -229,8 +173,13 @@ function calcParams(x1, y1, x2, y2, x3, y3) {
   document.getElementById("x0h").value = orthocenter.x;
   document.getElementById("y0h").value = orthocenter.y;
 
-  // 傍心 Excenter の位置計算
+  /**
+   * 傍心 Excenter の位置計算
+   */
+
+  // 3つの傍心のPoint objectを格納する変数
   var excenter = {};
+
   excenter.a = new Point(
     (b * x2 + c * x3 - a * x1) / (b + c - a),
     (b * y2 + c * y3 - a * y1) / (b + c - a)
@@ -258,9 +207,15 @@ function calcParams(x1, y1, x2, y2, x3, y3) {
   document.getElementById("y0ic").value = excenter.c.y;
   document.getElementById("rc").value = excenter.c.radius;
 
-  // 三角形各辺の延長線がCanvasエッジと交わる点の座標を計算
-  // 引数および返り値: Pointクラスで表記された、２つの座標
+  /**
+   * 三角形各辺の延長線がCanvasエッジと交わる点の座標を計算
+   * @param {Object} vertex1 頂点のPoint object
+   * @param {Object} vertex2 頂点のPoint object
+   * @return {Object} ２つの交点のPoint Object
+   */
   function getEdgePoints(vertex1, vertex2) {
+    // 長方形のCanvas領域を横切る直線は必ず２つの交点を持つので
+    // ２つのPoint objectがこれに格納される
     let edgePoints = [];
 
     if (typeof vertex1.x === "undefined" || typeof vertex2.x === "undefined") {
@@ -330,258 +285,300 @@ function calcParams(x1, y1, x2, y2, x3, y3) {
 }
 
 // 内心の変数を受け取り、それに則って描写
-function drawIncenter(incenter, r, R, S, x1, y1, x2, y2, x3, y3, ctx) {
+function drawIncenter(params, ctx) {
   // cicle
   ctx.beginPath();
-  ctx.arc(incenter.x, incenter.y, r, 0, 2 * Math.PI);
+  ctx.arc(params.incenter.x, params.incenter.y, params.r, 0, 2 * Math.PI);
   ctx.strokeStyle = "green";
   ctx.stroke();
 
   // center
   ctx.beginPath();
-  ctx.arc(incenter.x, incenter.y, 2, 0, 2 * Math.PI);
+  ctx.arc(params.incenter.x, params.incenter.y, 2, 0, 2 * Math.PI);
   ctx.fillStyle = "green";
   ctx.fill();
 }
 
 // 外心の変数を受け取り、それに則って描写
-function drawCircumcenter(circumcenter, r, R, S, x1, y1, x2, y2, x3, y3, ctx) {
+function drawCircumcenter(params, ctx) {
   // circle
   ctx.beginPath();
-  ctx.arc(circumcenter.x, circumcenter.y, R, 0, 2 * Math.PI);
+  ctx.arc(
+    params.circumcenter.x,
+    params.circumcenter.y,
+    params.R,
+    0,
+    2 * Math.PI
+  );
   ctx.strokeStyle = "skyblue";
   ctx.stroke();
 
   // center
   ctx.beginPath();
-  ctx.arc(circumcenter.x, circumcenter.y, 2, 0, 2 * Math.PI);
+  ctx.arc(params.circumcenter.x, params.circumcenter.y, 2, 0, 2 * Math.PI);
   ctx.fillStyle = "skyblue";
   ctx.fill();
 }
 
 // 垂心の変数を受け取り、それに則って描写
-function drawOrthocenter(orthocenter, r, R, S, x1, y1, x2, y2, x3, y3, ctx) {
+function drawOrthocenter(params, x1, y1, x2, y2, x3, y3, ctx) {
   // perpendicular line 1
   ctx.beginPath();
-  ctx.moveTo(orthocenter.x, orthocenter.y);
+  ctx.moveTo(params.orthocenter.x, params.orthocenter.y);
   ctx.lineTo(x1, y1);
   ctx.strokeStyle = "orange";
   ctx.stroke();
 
   // perpendicular line 2
   ctx.beginPath();
-  ctx.moveTo(orthocenter.x, orthocenter.y);
+  ctx.moveTo(params.orthocenter.x, params.orthocenter.y);
   ctx.lineTo(x2, y2);
   ctx.strokeStyle = "orange";
   ctx.stroke();
 
   // perpendicular line 3
   ctx.beginPath();
-  ctx.moveTo(orthocenter.x, orthocenter.y);
+  ctx.moveTo(params.orthocenter.x, params.orthocenter.y);
   ctx.lineTo(x3, y3);
   ctx.strokeStyle = "orange";
   ctx.stroke();
 
   // center
   ctx.beginPath();
-  ctx.arc(orthocenter.x, orthocenter.y, 2, 0, 2 * Math.PI);
+  ctx.arc(params.orthocenter.x, params.orthocenter.y, 2, 0, 2 * Math.PI);
   ctx.fillStyle = "orange";
   ctx.fill();
 }
 
 // 重心の変数を受け取り、それに則って描写
-function drawCentroid(centroid, r, R, S, x1, y1, x2, y2, x3, y3, ctx) {
+function drawCentroid(params, x1, y1, x2, y2, x3, y3, ctx) {
   // bisector line 1
   ctx.beginPath();
   ctx.moveTo(x1, y1);
-  ctx.lineTo(centroid.x, centroid.y);
+  ctx.lineTo(params.centroid.x, params.centroid.y);
   ctx.strokeStyle = "blue";
   ctx.stroke();
 
   // bisector line 2
   ctx.beginPath();
   ctx.moveTo(x2, y2);
-  ctx.lineTo(centroid.x, centroid.y);
+  ctx.lineTo(params.centroid.x, params.centroid.y);
   ctx.strokeStyle = "blue";
   ctx.stroke();
 
   // bisector line 3
   ctx.beginPath();
   ctx.moveTo(x3, y3);
-  ctx.lineTo(centroid.x, centroid.y);
+  ctx.lineTo(params.centroid.x, params.centroid.y);
   ctx.strokeStyle = "blue";
   ctx.stroke();
 
   // center
   ctx.beginPath();
-  ctx.arc(centroid.x, centroid.y, 2, 0, 2 * Math.PI);
+  ctx.arc(params.centroid.x, params.centroid.y, 2, 0, 2 * Math.PI);
   ctx.fillStyle = "blue";
   ctx.fill();
 }
 
-// 傍心の変数を受け取り、それに則って描写
-function drawExcenter(
-  excenter,
-  r,
-  R,
-  S,
-  edgePoints,
-  x1,
-  y1,
-  x2,
-  y2,
-  x3,
-  y3,
-  ctx
-) {
-  // circle 1
+/**
+ * 傍心の変数を受け取り、それに則って描画
+ *
+ * @param {*} params
+ * @param {*} x1
+ * @param {*} y1
+ * @param {*} x2
+ * @param {*} y2
+ * @param {*} x3
+ * @param {*} y3
+ * @param {*} ctx
+ */
+function drawExcenter(params, x1, y1, x2, y2, x3, y3, ctx) {
+  // circle A
   ctx.beginPath();
-  ctx.arc(excenter.a.x, excenter.a.y, excenter.a.radius, 0, 2 * Math.PI);
+  ctx.arc(
+    params.excenter.a.x,
+    params.excenter.a.y,
+    params.excenter.a.radius,
+    0,
+    2 * Math.PI
+  );
   ctx.strokeStyle = "purple";
   ctx.stroke();
 
-  // circle 2
+  // circle B
   ctx.beginPath();
-  ctx.arc(excenter.b.x, excenter.b.y, excenter.b.radius, 0, 2 * Math.PI);
+  ctx.arc(
+    params.excenter.b.x,
+    params.excenter.b.y,
+    params.excenter.b.radius,
+    0,
+    2 * Math.PI
+  );
   ctx.strokeStyle = "purple";
   ctx.stroke();
 
-  // circle 3
+  // circle C
   ctx.beginPath();
-  ctx.arc(excenter.c.x, excenter.c.y, excenter.c.radius, 0, 2 * Math.PI);
+  ctx.arc(
+    params.excenter.c.x,
+    params.excenter.c.y,
+    params.excenter.c.radius,
+    0,
+    2 * Math.PI
+  );
   ctx.strokeStyle = "purple";
   ctx.stroke();
 
-  // vertex-to-excenter line 1
+  // Line from vertex 1 to Excenter A
   ctx.beginPath();
-  ctx.moveTo(excenter.a.x, excenter.a.y);
+  ctx.moveTo(params.excenter.a.x, params.excenter.a.y);
   ctx.lineTo(x1, y1);
   ctx.strokeStyle = "plum";
   ctx.stroke();
 
-  // vertex-to-excenter line 2
+  // Line from vertex 2 to Excenter B
   ctx.beginPath();
-  ctx.moveTo(excenter.b.x, excenter.b.y);
+  ctx.moveTo(params.excenter.b.x, params.excenter.b.y);
   ctx.lineTo(x2, y2);
   ctx.strokeStyle = "plum";
   ctx.stroke();
 
-  // vertex-to-excenter line 3
+  // Line from vertex 3 to Excenter C
   ctx.beginPath();
-  ctx.moveTo(excenter.c.x, excenter.c.y);
+  ctx.moveTo(params.excenter.c.x, params.excenter.c.y);
   ctx.lineTo(x3, y3);
   ctx.strokeStyle = "plum";
   ctx.stroke();
 
-  // center 1
+  // Excenter A
   ctx.beginPath();
-  ctx.arc(excenter.a.x, excenter.a.y, 2, 0, 2 * Math.PI);
+  ctx.arc(params.excenter.a.x, params.excenter.a.y, 2, 0, 2 * Math.PI);
   ctx.fillStyle = "purple";
   ctx.fill();
 
-  // center 2
+  // Excenter B
   ctx.beginPath();
-  ctx.arc(excenter.b.x, excenter.b.y, 2, 0, 2 * Math.PI);
+  ctx.arc(params.excenter.b.x, params.excenter.b.y, 2, 0, 2 * Math.PI);
   ctx.fillStyle = "purple";
   ctx.fill();
 
-  // center 3
+  // Excenter C
   ctx.beginPath();
-  ctx.arc(excenter.c.x, excenter.c.y, 2, 0, 2 * Math.PI);
+  ctx.arc(params.excenter.c.x, params.excenter.c.y, 2, 0, 2 * Math.PI);
   ctx.fillStyle = "purple";
   ctx.fill();
 
-  // excenter-to-excenter line 1
+  // Line from Excenter A to Excenter B
   ctx.beginPath();
-  ctx.moveTo(excenter.a.x, excenter.a.y);
-  ctx.lineTo(excenter.b.x, excenter.b.y);
+  ctx.moveTo(params.excenter.a.x, params.excenter.a.y);
+  ctx.lineTo(params.excenter.b.x, params.excenter.b.y);
   ctx.strokeStyle = "plum";
   ctx.stroke();
 
-  // excenter-to-excenter line 2
+  // Line from Excenter B to Excenter C
   ctx.beginPath();
-  ctx.moveTo(excenter.b.x, excenter.b.y);
-  ctx.lineTo(excenter.c.x, excenter.c.y);
+  ctx.moveTo(params.excenter.b.x, params.excenter.b.y);
+  ctx.lineTo(params.excenter.c.x, params.excenter.c.y);
   ctx.strokeStyle = "plum";
   ctx.stroke();
 
-  // excenter-to-excenter line 3
+  // Line from Excenter C to Excenter A
   ctx.beginPath();
-  ctx.moveTo(excenter.c.x, excenter.c.y);
-  ctx.lineTo(excenter.a.x, excenter.a.y);
+  ctx.moveTo(params.excenter.c.x, params.excenter.c.y);
+  ctx.lineTo(params.excenter.a.x, params.excenter.a.y);
   ctx.strokeStyle = "plum";
   ctx.stroke();
 
-  // 接線の描写
+  // 傍心円の接線を描画
+  // 三角形が完成し、その各辺の延長線位置が判明しているときのみ描画
   if (
-    typeof edgePoints.a !== "undefined" &&
-    typeof edgePoints.b !== "undefined" &&
-    typeof edgePoints.c !== "undefined"
+    typeof params.edgePoints.a !== "undefined" &&
+    typeof params.edgePoints.b !== "undefined" &&
+    typeof params.edgePoints.c !== "undefined"
   ) {
     // 破線にする
     ctx.setLineDash([1, 2]);
 
     ctx.beginPath();
-    ctx.moveTo(edgePoints.a[0].x, edgePoints.a[0].y);
-    ctx.lineTo(edgePoints.a[1].x, edgePoints.a[1].y);
+    ctx.moveTo(params.edgePoints.a[0].x, params.edgePoints.a[0].y);
+    ctx.lineTo(params.edgePoints.a[1].x, params.edgePoints.a[1].y);
     ctx.strokeStyle = "purple";
     ctx.stroke();
 
     ctx.beginPath();
-    ctx.moveTo(edgePoints.b[0].x, edgePoints.b[0].y);
-    ctx.lineTo(edgePoints.b[1].x, edgePoints.b[1].y);
+    ctx.moveTo(params.edgePoints.b[0].x, params.edgePoints.b[0].y);
+    ctx.lineTo(params.edgePoints.b[1].x, params.edgePoints.b[1].y);
     ctx.strokeStyle = "purple";
     ctx.stroke();
 
     ctx.beginPath();
-    ctx.moveTo(edgePoints.c[0].x, edgePoints.c[0].y);
-    ctx.lineTo(edgePoints.c[1].x, edgePoints.c[1].y);
+    ctx.moveTo(params.edgePoints.c[0].x, params.edgePoints.c[0].y);
+    ctx.lineTo(params.edgePoints.c[1].x, params.edgePoints.c[1].y);
     ctx.strokeStyle = "purple";
     ctx.stroke();
 
-    // 破線設定を実線に戻す
+    // これ以降の線のスタイルを実線に戻す
     ctx.setLineDash([]);
   }
 }
 
-// 五心の変数を受け取り、それに則ってオイラー線を描写
-function drawEulerLine(ctx, circumcenter, centroid, orthocenter) {
+/**
+ * 五心の変数を受け取り、それに則ってオイラー線を描写
+ *
+ * @param {*} ctx Canvas Context
+ * @param {*} params 五心の各座標などの計算結果
+ */
+function drawEulerLine(ctx, params) {
   // 外心-垂心の結合線
   ctx.beginPath();
-  ctx.moveTo(orthocenter.x, orthocenter.y);
-  ctx.lineTo(circumcenter.x, circumcenter.y);
+  ctx.moveTo(params.orthocenter.x, params.orthocenter.y);
+  ctx.lineTo(params.circumcenter.x, params.circumcenter.y);
   ctx.strokeStyle = "red";
   ctx.stroke();
 
   // 重心-外心の結合線
   ctx.beginPath();
-  ctx.moveTo(circumcenter.x, circumcenter.y);
-  ctx.lineTo(centroid.x, centroid.y);
+  ctx.moveTo(params.circumcenter.x, params.circumcenter.y);
+  ctx.lineTo(params.centroid.x, params.centroid.y);
   ctx.strokeStyle = "red";
   ctx.stroke();
 
   // 重心-垂心の結合線
   ctx.beginPath();
-  ctx.moveTo(centroid.x, centroid.y);
-  ctx.lineTo(orthocenter.x, orthocenter.y);
+  ctx.moveTo(params.centroid.x, params.centroid.y);
+  ctx.lineTo(params.orthocenter.x, params.orthocenter.y);
   ctx.strokeStyle = "red";
   ctx.stroke();
 }
 
-// Handle the free triangle input by user
+/**
+ * ユーザのクリックによる任意の三角形指定のハンドラ
+ * クリック毎に呼ばれる
+ * @param {*} e Click event
+ */
 function freeClick(e) {
   var x = e.clientX - canvas.offsetLeft;
   var y = e.clientY - canvas.offsetTop;
-  clickLoc.x.push(x);
-  clickLoc.y.push(y);
+  clickPoints.push(new Point(x, y));
 
-  var x1 = clickLoc.x[0];
-  var y1 = clickLoc.y[0];
-  var x2 = clickLoc.x[1];
-  var y2 = clickLoc.y[1];
-  var x3 = clickLoc.x[2];
-  var y3 = clickLoc.y[2];
+  // clickPoints配列に３頂点分揃っていない時は
+  // 単に無視して何も代入しないよう条件分岐
+  if (clickPoints[0]) {
+    var x1 = clickPoints[0].x;
+    var y1 = clickPoints[0].y;
+  }
 
+  if (clickPoints[1]) {
+    var x2 = clickPoints[1].x;
+    var y2 = clickPoints[1].y;
+  }
+
+  if (clickPoints[2]) {
+    var x3 = clickPoints[2].x;
+    var y3 = clickPoints[2].y;
+  }
+
+  // 二点目以降がまだ指定されていない時は、座標値はundefinedとなる
   document.getElementById("x1").value = x1;
   document.getElementById("y1").value = y1;
   document.getElementById("x2").value = x2;
@@ -589,17 +586,16 @@ function freeClick(e) {
   document.getElementById("x3").value = x3;
   document.getElementById("y3").value = y3;
 
+  // 点を随時描画する。３点揃った時は、五心全てが描画される
   draw(x1, y1, x2, y2, x3, y3);
 
-  // Empty the click history when clicked 3 times
-  // so that it can accept future input
-  if (clickLoc.x.length == 3) {
-    clickLoc.x = [];
-    clickLoc.y = [];
-  }
+  // 3つの点がクリックされたら、用済みなので履歴をクリアし将来のクリックに備える
+  if (clickPoints.length == 3) clickPoints = [];
 }
 
-// Draw 直角三角形 Right triangle
+/**
+ *  直角三角形 Right triangleの自動生成
+ */
 function generateRightTriangle() {
   var ran = Math.random() * (1024 - 256);
 
@@ -620,7 +616,7 @@ function generateRightTriangle() {
   draw(x1, y1, x2, y2, x3, y3);
 }
 
-// Draw 正三角形 Equilateral triangle
+// 正三角形 Equilateral triangleの生成
 function generateEquilateralTriangle() {
   x1 = 200;
   y1 = 200;
@@ -639,7 +635,9 @@ function generateEquilateralTriangle() {
   draw(x1, y1, x2, y2, x3, y3);
 }
 
-// Draw random triangle
+/**
+ * 形状がランダムな三角形の自動生成
+ */
 function generateRandomTriangle() {
   let x1, y1, x2, y2, x3, y3;
 
@@ -651,6 +649,7 @@ function generateRandomTriangle() {
   x3 = CANVAS_WIDTH / 4 + (Math.random() * CANVAS_WIDTH) / 2;
   y3 = CANVAS_HEIGHT / 4 + (Math.random() * CANVAS_HEIGHT) / 2;
 
+  // 生成値をinput formに表示
   document.getElementById("x1").value = x1;
   document.getElementById("y1").value = y1;
   document.getElementById("x2").value = x2;
@@ -661,7 +660,11 @@ function generateRandomTriangle() {
   draw(x1, y1, x2, y2, x3, y3);
 }
 
-// Render only one center (centroid, incenter, etc.)
+/**
+ * 五心のうち指定されたものだけを描写する
+ * 三角形頂点の座標はHTMLの入力欄から取得する
+ * @param {string} content "incenter", "excenter"のような五心の指定（省略可）
+ */
 function renderCircle(content) {
   let x1 = Number(document.getElementById("x1").value);
   let y1 = Number(document.getElementById("y1").value);
@@ -673,4 +676,5 @@ function renderCircle(content) {
   draw(x1, y1, x2, y2, x3, y3, content);
 }
 
+// Canvasへのクリックイベントに対するリスナー
 canvas.addEventListener("click", freeClick, false);
